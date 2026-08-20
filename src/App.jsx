d5bc14108768bf8ba5716254e0c76bee4da16567
User-MiMa <mlsmasson@outlook.com>
2026-08-19 20:38:27 -0600

    feat: add react icon library; add svg to Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Button from "./components/Button";
+import {FaMoneyBill} from "react-icons/fa"
 
 export default function App(){
     return(
         <main>
-            <Button>Buy now!</Button>
+            <Button>
+                <FaMoneyBill/>
+                Buy now!
+            </Button>
         </main>
     );
 }
