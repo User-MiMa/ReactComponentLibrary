f310704f3182935fa47eec0791aa8bf60ebfb286
User-MiMa <mlsmasson@outlook.com>
2026-09-02 17:03:39 -0600

    feat: add Avatar component with style and react-icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Menu from "./components/Menu/Menu";
 import MenuButton from "./components/Menu/MenuButton";
 import MenuDropdown from "./components/Menu/MenuDropdown";
 import MenuDropdownItem from "./components/Menu/MenuDropdownItem";
+import Avatar from "./components/Avatar/Avatar";
 
 export default function App(){
 
@@ -15,12 +16,21 @@ export default function App(){
                 <FaMoneyBill/>
                 Buy now!
             </Button>
+        <br />
             <Menu>
                 <MenuButton> Sports </MenuButton>
                 <MenuDropdown> 
                     {sports.map(sport=>(<MenuDropdownItem key={sport}>{sport}</MenuDropdownItem>))} 
                 </MenuDropdown>
             </Menu>
+        <br />
+        <br />
+            <Avatar />
+        <br />
+            <Avatar src="./imgs/pfp.png" alt="Profile Picture" />
+        <br />
+            <Avatar>MM</Avatar>
+        <br />
         </main>
     );
 }
